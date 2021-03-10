cddc7a29e42859cda9844867d707e88211f9cedb
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-11 00:41:33 +0100

    create test that checks if the board is printing correctly

--- src/test/java/com/epam/academy/ox/OXTest.java
+++ src/test/java/com/epam/academy/ox/OXTest.java
@@ -1,14 +1,45 @@
 package com.epam.academy.ox;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.testng.Assert.*;
 
 public class OXTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeMethod
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterMethod
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 
     @Test
     public void shouldCreateCertainSizeBoard() {
         Board board = new Board(9);
         assertEquals(board.grid.length, 9);
+    }
+
+    @Test
+    public void shouldPrintBoardCorrectly() {
+        Board board = new Board(9);
+        String expected = "1 2 3\n4 5 6\n7 8 9";
+
+        board.print();
+
+        assertEquals(outContent.toString(),expected);
     }
 }
