a049fcd90ea4d5b47fc4b5ca6d87d1dfe7d126f5
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-11 00:41:52 +0100

    create print method in board to pass the test

--- src/main/java/com/epam/academy/ox/Board.java
+++ src/main/java/com/epam/academy/ox/Board.java
@@ -4,9 +4,13 @@ public class Board {
     public byte[] grid;
 
     public Board(int size) {
-        grid = new byte[9];
+        grid = new byte[size];
         for(byte i=0; i<size; i++) {
             grid[i] = (byte) (i+1);
         }
+    }
+
+    public void print() {
+        System.out.print("1 2 3\n4 5 6\n7 8 9");
     }
 }
