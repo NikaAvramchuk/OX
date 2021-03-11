22856a7fcaaaf5b4005a8c58e9b72496af8b3a6f
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-11 01:49:14 +0100

    create Player class and method chooseSlot inside it

---
+++ src/main/java/com/epam/academy/ox/Player.java
@@ -0,0 +1,13 @@
+package com.epam.academy.ox;
+
+public class Player {
+    char name;
+
+    public Player(char name) {
+        this.name = name;
+    }
+
+    public int chooseSlot() {
+        return 5;
+    }
+}
