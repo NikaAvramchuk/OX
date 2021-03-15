80efd05694fd06cf6348f36f5c6fb876668e6b77
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-15 12:09:04 +0100

    update checkWinner method

--- src/main/java/com/epam/academy/ox/Game.java
+++ src/main/java/com/epam/academy/ox/Game.java
@@ -1,7 +1,25 @@
 package com.epam.academy.ox;
 
+import java.util.Arrays;
+
+import static com.epam.academy.ox.Combination.winningCombinations;
+
 public class Game {
-    public Player checkWinner() {
-        return new Player('X');
+    private final Player playerX = new Player('X');
+    private final Player playerO = new Player('O');
+
+    public Player checkWinner(Board board) {
+        StringBuilder result = new StringBuilder();
+        for (int[] winningCombination : winningCombinations) {
+            for (int i : winningCombination)
+                result.append(board.grid[i]);
+            if(result.toString().equals("XXX"))
+                return playerX;
+            else if(result.toString().equals("OOO"))
+                return playerO;
+            result = new StringBuilder();
+        }
+        return null;
     }
+
 }
