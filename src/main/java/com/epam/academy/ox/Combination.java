f02b3aeaa93fd96b1857b7c60e82a257485b8982
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-15 12:07:35 +0100

    create Combination class that holds the coordinates of all winning combinations

---
+++ src/main/java/com/epam/academy/ox/Combination.java
@@ -0,0 +1,14 @@
+package com.epam.academy.ox;
+
+public class Combination {
+    static int[][] winningCombinations = new int[][] {
+            {0,1,2},
+            {0,4,8},
+            {0,3,6},
+            {2,5,8},
+            {2,4,6},
+            {6,7,8},
+            {3,4,5}
+    };
+
+}
